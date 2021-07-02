f87aae85078c02fcdb088c5ebf119e0714c78bc0
Mak2503 <ak2955702@gmail.com>
2021-07-02 20:43:30 +0530

    add json and api

--- translate.js
+++ translate.js
@@ -1,9 +1,17 @@
 var txtInput = document.querySelector("#txt-input");
 var btnTranslate = document.querySelector("#btn-translate");
 var txtOutput = document.querySelector("#txt-output");
+var transUrl = "https://api.funtranslations.com/translate/yoda.json";
 
 function clicked(){
-    console.log("clicked");
+    var takeInput = transUrl + "?text=" + txtInput.value;
+    fetch(takeInput)
+    .then(response => response.json())
+    .then(json => {
+        var outputTxt = json.contents.translated;
+        txtOutput.innerText = outputTxt;
+    })
+    .catch(error => alert("the server is down. please try again in sometime"));
 }
 
 btnTranslate.addEventListener("click", clicked);
