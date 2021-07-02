84cbb08c3ac64d4a5298fd925901b91b3e014619
Mak2503 <ak2955702@gmail.com>
2021-07-02 19:42:28 +0530

    add js file

---
+++ translate.js
@@ -0,0 +1,9 @@
+var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtOutput = document.querySelector("#txt-output");
+
+function clicked(){
+    console.log("clicked");
+}
+
+btnTranslate.addEventListener("click", clicked);
