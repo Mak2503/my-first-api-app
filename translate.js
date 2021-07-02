fae55e841e10bb833634a9784c2d74365680cb1d
Mak2503 <ak2955702@gmail.com>
2021-07-02 20:59:00 +0530

    minor fix

--- translate.js
+++ translate.js
@@ -3,15 +3,15 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtOutput = document.querySelector("#txt-output");
 var transUrl = "https://api.funtranslations.com/translate/yoda.json";
 
-function clicked(){
+function clicked() {
     var takeInput = transUrl + "?text=" + txtInput.value;
     fetch(takeInput)
-    .then(response => response.json())
-    .then(json => {
-        var outputTxt = json.contents.translated;
-        txtOutput.innerText = outputTxt;
-    })
-    .catch(error => alert("the server is down. please try again in sometime"));
+        .then(response => response.json())
+        .then(json => {
+            var outputTxt = json.contents.translated;
+            txtOutput.innerText = outputTxt;
+        })
+        .catch(error => alert("An error occured. Please try again in sometime"));
 }
 
 btnTranslate.addEventListener("click", clicked);
